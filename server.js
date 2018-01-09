51cf74b8e1e1deb4af11aad7776feef186fcc29a
Gilad Novik <gilad@novik.ca>
2018-01-09 17:16:34 +0200

    more info

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 const express = require('express');
 
-express().get('/', (req, res)=>res.send('ok...')).listen(8080, ()=>console.log('listening...'));
+express().use((req, res)=>res.json({
+    url: req.url,
+    headers: req.headers,
+    env: process.env,
+})).listen(process.env.PORT||3000, ()=>console.log('listening...'));
 
 
